31881ab638022a461895263ecccd8ce09d7a5727
Rodrigo Augusto Casemiro <rodrigo.casemiro@spread.com.br>
2023-06-29 18:13:33 -0300

    create cypress project

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -14,6 +14,28 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     it('verifica o título da aplicação', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
+    it('Preenchi e envia formulário', function() {
+        cy.fillMandatoryFieldsAndSubmit()
+    })
+    it('',function(){
+        cy.marcaTodosDesmarcaTodos()
+    })
+    it('seleciona um arquivo da pasta fixtures', function() {
+        cy.get('input[type="file"]')
+            .selectFile('./cypress/fixtures/example.json', {action: 'drag-drop'})
+            .should(function($input){
+                console.log($input)
+            })
+        //selectFile()
+    })
+    it.only('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', function(){
+        cy.fixture('example.json').as('sampleFile')
+        cy.get('input[type="file"]')
+            .selectFile('@sampleFile')
+            .should(function($input){
+                expect($input[0].files[0].name).to.equal('example.json')
+            })
+    })
   })
 
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,39 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', function(){
+    var Telefone = 11988468495
+    var Texto = 'Não tenho nada a comentar'
+    
+    cy.get('#firstName').type('Rodrigo').should('have.be','Rodrigo')
+    cy.get('#lastName').type('Augusto Casemiro').should('have.be','Augusto Casemiro')
+    cy.get('#phone').type(Telefone)
+    cy.get('#email').type('rodrigo_casemiro@live.com').should('have.be', 'rodrigo.casemiro@live,com')
+    cy.get('#open-text-area').type(Texto, {delay: 20})
+    cy.get('#product').select(1).wait(1000)
+    cy.get('input[type="radio"]')
+        .wait(1000)
+            .each(function(){
+                cy.get('input[type="radio"]').check('elogio')
+            })
+
+    cy.get('input[type="checkbox"]')
+        .check('phone')
+        .wait(1000)
+    cy.contains('.button','Enviar')
+        .click()
+    cy.get('.success')
+        .should('be.visible')
+    //cy.get('.error').should('be.visible')
+    //.clear()
+});
+
+Cypress.Commands.add('marcaTodosDesmarcaTodos', function(){
+    cy.get('input[type="checkbox"]')
+        .check()
+        .should('be.checked')
+    cy.get('input[type="checkbox"]')
+        .uncheck()
+        .should('not.be.checked')
+})
